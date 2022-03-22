62991bd4ab16ee6e40c8b070dac5ff015471214f
Rudra Goel <goelrudra2016@gmail.com>
2022-03-21 22:56:17 -0600

    changed api urls

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 //each url is associated with its own CRUD opp. this one on the following line only returns all the posts because in localhost:1337/posts/ returns all the posts from the backend
 //we would need to make more urls and ths create more arrow functions and export them for the frontend to use 
-const url = 'http://localhost:1337/posts'
-const urlUsers = 'http://localhost:1337/users'
+const url = 'https://view-rado.herokuapp.com/posts'
+const urlUsers = 'https://view-rado.herokuapp.com/users'
 
 export const fetchLocationsBasicSearch = (params) => axios.post(url, params)
 export const fetchLocationsAdvancedSearch = (params, page) => axios.post(url+`/advanced?page=${page}`, params)
