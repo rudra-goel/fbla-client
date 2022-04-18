181ead9e2c10722d278fee3c6e656a2ec57e215a
mimi-rai <66657076+mimi-rai@users.noreply.github.com>
2022-04-18 05:24:06 -0600

    change description

--- src/pages/Authentication/Register.js
+++ src/pages/Authentication/Register.js
@@ -85,7 +85,7 @@ function App() {
             /**
              * Set the loading state to true to activate the circular loading wheel to indicate the process has started for registration with the database
              */
-            setLoading(true)
+            setLoading(false)
             /**
              * The function to register the user is called where we pass in the data attempted and an object reference to the useNavigate Hook
              * The details of the authenticated user (token, name, email, etc.) are then piped to the global state of variables via dispatch function
@@ -94,8 +94,7 @@ function App() {
 
            
         } catch (error) {
-            console.log(error)
-           
+            console.log(error)        
         }
     }
 
