9990535e5d2f3d57dbb60888ec971537223aa8f0
mimi-rai <66657076+mimi-rai@users.noreply.github.com>
2022-04-18 05:06:18 -0600

    wtf

--- src/pages/Authentication/Register.js
+++ src/pages/Authentication/Register.js
@@ -83,16 +83,17 @@ function App() {
                 console.log("passwords aren't matching")
                 window.alert("passwords are not matching")
             }
-
-            /**
-             * Set the loading state to true to activate the circular loading wheel to indicate the process has started for registration with the database
-             */
-            setLoading(true)
-            /**
-             * The function to register the user is called where we pass in the data attempted and an object reference to the useNavigate Hook
-             * The details of the authenticated user (token, name, email, etc.) are then piped to the global state of variables via dispatch function
-             */
-            dispatch(registerUser(postData, history)) 
+            else {
+                /**
+                 * Set the loading state to true to activate the circular loading wheel to indicate the process has started for registration with the database
+                 */
+                setLoading(true)
+                /**
+                 * The function to register the user is called where we pass in the data attempted and an object reference to the useNavigate Hook
+                 * The details of the authenticated user (token, name, email, etc.) are then piped to the global state of variables via dispatch function
+                 */
+                dispatch(registerUser(postData, history)) 
+            }
         } catch (error) {
             console.log(error)
             console.log("error")
